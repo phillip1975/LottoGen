5148656b6fd3095c234a9d6b70a6c9e1bb1d6db5
unsinn <fabian@ma3.ch>
2014-08-25 16:45:31 +0200

    * Each number now appears only once per Tip * Prettier print

--- LottoGen/src/main/java/ch/hslu/lotto/Tip.java
+++ LottoGen/src/main/java/ch/hslu/lotto/Tip.java
@@ -1,28 +1,28 @@
 package ch.hslu.lotto;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Tip {
 
 	private List<Integer> values;
-	private Random rand;
 
 	public List<Integer> getValueList() {
 		return values;
 	}
 
 	public void generate(int nrOfValues, int min, int max) {
-		values = Arrays.asList(new Integer[nrOfValues]);
-		rand = new Random();
-		values.replaceAll(v -> rand.nextInt(max) + min);
+		List<Integer> valueRange = IntStream.range(min, max+1).boxed().collect(Collectors.toList());
+		Collections.shuffle(valueRange);
+		values = valueRange.stream().limit(nrOfValues).collect(Collectors.toList());
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder s = new StringBuilder();
-		values.stream().sorted().forEach(v -> s.append(" " + v));
+		values.stream().sorted().forEach(v -> s.append(String.format("%3d", v)));
 		return s.toString();
 	}
 }
